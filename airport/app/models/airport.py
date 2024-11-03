b17f8d51be746ccc18ddc1b71042e01850ad197b
BrianKimurgor <kimurgorbrian20@gmail.com>
2024-11-03 20:37:36 +0300

    added comments

--- airport/app/models/airport.py
+++ airport/app/models/airport.py
@@ -1,6 +1,29 @@
 from mongoengine import Document, StringField, IntField, ListField
 
 class Airport(Document):
+    """
+    A class to represent an Airport.
+
+    Attributes
+    ----------
+    name : StringField
+        The name of the airport. This field is required.
+    location : StringField
+        The location of the airport. This field is required.
+    code : StringField
+        The unique code of the airport. This field is required and must be unique.
+    gates : IntField
+        The number of gates in the airport. This field is required and must be at least 1.
+    terminals : IntField
+        The number of terminals in the airport. This field is required and must be at least 1.
+    facilities : ListField
+        A list of facilities available at the airport. Defaults to an empty list.
+
+    Meta
+    ----
+    collection : str
+        The name of the collection in the database where airport documents are stored.
+    """
     name = StringField(required=True)
     location = StringField(required=True)
     code = StringField(required=True, unique=True)
--- airport/app/models/user.py
+++ airport/app/models/user.py
@@ -2,14 +2,45 @@ from mongoengine import Document, StringField, BooleanField
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(Document):
+    """
+    A class to represent a user in the system.
+
+    Attributes:
+    ----------
+    username : StringField
+        A unique and required string field for the user's username.
+    password_hash : StringField
+        A required string field to store the hashed password of the user.
+    admin : BooleanField
+        A boolean field to indicate if the user has admin privileges, defaults to False.
+    """
     username = StringField(required=True, unique=True)
     password_hash = StringField(required=True)
     admin = BooleanField(default=False)
 
     def set_password(self, password):
-        """Hashes the password and stores it in the password_hash field."""
+        """
+        Hashes the password and stores it in the password_hash field.
+
+        Parameters:
+        ----------
+        password : str
+            The plain text password to be hashed and stored.
+        """
         self.password_hash = generate_password_hash(password)
     
     def check_password(self, password):
-        """Checks if the provided password matches the stored password hash."""
+        """
+        Checks if the provided password matches the stored password hash.
+
+        Parameters:
+        ----------
+        password : str
+            The plain text password to be checked against the stored hash.
+
+        Returns:
+        -------
+        bool
+            True if the password matches the stored hash, False otherwise.
+        """
         return check_password_hash(self.password_hash, password)
