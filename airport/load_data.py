2ea2dbbd5a592a44a0753205d0fcb10e537945b1
BrianKimurgor <kimurgorbrian20@gmail.com>
2024-11-03 06:13:55 +0300

    added descriptive comment

--- airport/load_data.py
+++ airport/load_data.py
@@ -8,7 +8,23 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client.airport_app
 
 def load_data():
-    # Load users
+    """
+    Load user and airport data from JSON files into the database.
+
+    This function performs the following operations:
+    - Loads user data from 'airport_users.json' and inserts each user into the 'users' collection
+        in the database. If a user with the same username already exists, that user is skipped.
+        Passwords are hashed before insertion.
+    - Loads airport data from 'airports.json' and inserts each airport into the 'airports' collection
+        in the database. If an airport with the same code already exists, that airport is skipped.
+        Each airport is assigned a new [ObjectId](https://www.mongodb.com/docs/manual/reference/object-id/), and any existing 'id' field is removed.
+
+    Note:
+    - The JSON files are expected to be located in a 'data' directory relative to the script's location.
+    - The database operations assume the existence of 'users' and 'airports' collections.
+    - The function uses 'generate_password_hash' for password hashing and 'ObjectId' for generating
+        unique identifiers for airports.
+    """
     file_path = os.path.join(os.path.dirname(__file__), "data/airport_users.json")
     with open(file_path) as f:
         users = json.load(f)["airport_users"]
